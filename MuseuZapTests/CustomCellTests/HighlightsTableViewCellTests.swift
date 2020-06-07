f9d32030e57095ea0f1512c57f2bee07c3d07cab
Bernardo Silva <silva98.bernardo@gmail.com>
2020-06-07 16:27:31 -0300

    fix warnings that aren't TODOs

--- MuseuZapTests/CustomCellTests/HighlightsTableViewCellTests.swift
+++ MuseuZapTests/CustomCellTests/HighlightsTableViewCellTests.swift
@@ -1,4 +1,3 @@
-
 //
 //  HighlightsTableViewCellTestss.swift
 //  MuseuZapTests
--- MuseuZapTests/MyAudios/MyAudiosViewControllerTests.swift
+++
@@ -1,9 +0,0 @@
-////
-////  MyAudiosViewControllerTests.swift
-////  MuseuZapTests
-////
-////  Created by Ignácio Espinoso Ribeiro on 05/06/20.
-////  Copyright © 2020 Bernardo. All rights reserved.
-////
-//
-//import Foundation
